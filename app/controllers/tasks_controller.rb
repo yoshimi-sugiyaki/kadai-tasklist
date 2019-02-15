a5b43ab873e6aa4e8e1069159e05a94aca192861
yoshimi.sugiyaki <sugiyaki@niflon.co.jp>
2019-02-15 08:19:01 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,18 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @tasks = current_user.tasks.page(params[:page])
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
@@ -13,21 +24,34 @@ class TasksController < ApplicationController
       render 'toppages/index'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'メッセージを編集しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'メッセージの編集に失敗しました。'
+      render :edit
+    end
+  end
 
   def destroy
     @task.destroy
     flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_url
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
   def correct_user
-  @task = current_user.tasks.find_by(id: params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
+  root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
   
-  resources :tasks, only: [:create, :destroy]
+  resources :tasks
 end
